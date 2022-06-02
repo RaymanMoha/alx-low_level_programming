563ce0908d712d3a94435328656b49a28ae4b73f
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-02 12:00:32 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#iclude <stdio.h>
+
+int main(char *argv[], int argc)
+{
+/**
+ * main - Entry point
+ *
+ * Return : 1
+ */
+printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+Return (1);
+}
