21fc2461e5e6776cfa09accffc87e4b516c42427
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-04 13:13:35 -0700

    insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,20 +6,12 @@
  */
 int main(void)
 {
-	<<<<<<< HEAD
-	int num;
-	num = 97;
-	while (num <= 122)
-		putchar(num);
-	num++;
-	=======
-		int p;
-	p = 97;
-	while (p <= 122)
-	{
-		putchar(p);
-		p++;
-	}
-	>>>>>>> c38d008b18676f16774155151031a0fcc1c4675251031a0fcc1c46752
-		putchar('\n');
-	return (0);
+	char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+					putchar(letter);
+
+			putchar('\n');
+
+				return (0);
+}
