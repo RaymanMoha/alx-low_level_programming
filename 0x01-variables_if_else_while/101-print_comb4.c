0a87039ae56a5005c2397b5dfb3c92383626d29e
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-06 06:47:13 -0700

    ✨

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
-	
-	for (digit1 = 0; digit1 < 8; digit1++)
-	
-	{
+	int digit1, digit2, digit3;f
 		
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		for (digit1 = 0; digit1 < 8; digit1++)
+		
+		{
+			
+			for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		
 		{
 			
@@ -29,16 +29,14 @@ int main(void)
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					
 					continue;
-				
 				putchar(',');
 				putchar(' ');
-			
 			}
 		
 		}
 	}
-
-				putchar('\n');
-				
-				return (0);
+	
+	putchar('\n');
+	
+	return (0);
 }
