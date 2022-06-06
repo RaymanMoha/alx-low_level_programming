89e055c44b7a1bdf3c432b412389a635cff4203a
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-06 00:59:54 -0700

    whitespace

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,7 @@ int main(void)
 	char letter;
 	
 	for (letter = 'a'; letter <= 'z'; letter++)
-					putchar(letter);
-
-			putchar('\n');
-
-				return (0);
+		putchar(letter);
+		putchar('\n');
+		return (0);
 }
