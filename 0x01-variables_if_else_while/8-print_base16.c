9735d6c826d9350906e50335d565ccc5d8dc8a3e
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-06 05:37:38 -0700

    base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers0 to 9 and letters a to f
+ *
+ */
+int main(void)
+{
+	int X;
+
+	for (X = 48; X < 58; X++)
+	{
+		putchar(X);
+	}
+	for (X = 97; X < 103; X++)
+	{
+		putchar(X);
+	}
+	putchar('\n');
+
+	return (0);
+}
