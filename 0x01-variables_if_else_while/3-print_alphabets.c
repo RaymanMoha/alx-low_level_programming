3a49711c9ef9be939b89424a8f414c1e3463f457
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-06 01:33:22 -0700

    alphabest

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,8 +14,8 @@ int main(void)
 
 	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 		putchar(alphabets);
-	for (alphabets = 'A'; alphabet <= 'Z'; alphabet++)
-		putchar(alphabet);
+	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+		putchar(alphabets);
 	putchar('\n');
 
 	return (0);
