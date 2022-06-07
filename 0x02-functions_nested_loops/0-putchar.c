e8ffa2782b0b4149a43b05bb0bc46cda7ea36321
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-07 13:26:35 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,12 +12,12 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
+	char sh[8] = "_putchar";
+	int x;
 
-	for(i = 0; i < 8; i++)
+	for (x = 0; x < 8; i++)
 	{
-		_putchar(text[i]);
+		_putchar(sh[x]);
 	}
 	_putchar('\n');
 
