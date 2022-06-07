d365f0ed7c76f6a2b28bed93880401d8abc62b4d
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-07 08:13:28 -0700

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,13 @@
-#include "main.h"
+#include <main.h>
 #include <stdio.h>
 
 /**
  *
  *  * main - Entry point
  *
- *   *
+ * Return: nothing, return void
  *
- *    * Return: nothing, return void
- *
- *     */
+ */
 
 
 
