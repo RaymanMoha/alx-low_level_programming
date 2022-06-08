c8c280485856e622bcdc0b44766c1f350a47702d
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-08 03:11:43 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -17,14 +17,15 @@ int _isalpha(int c)
 		return (1);
 	}
 
-	if (c >= 65 && c <= 90)
+	else if (c >= 65 && c <= 90)
+	{
 
 		return (1);
-	{
-		else
-
 	}
 
-	return (0);
+	else
+	{
+		return (0);
+	}
 
 }
