ac83f53c892e852936d61e463ae261da287d782f
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-08 01:23:54 -0700

    x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+	int alphabet;
+	int b;
+
+	for (b = 1 b <= 10 b++)
+	{
+		for (alphabet = 'a', alphabet <= 'z' alphabet++)
+
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
