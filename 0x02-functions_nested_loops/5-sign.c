389f1f731ebd2d73e7a0d41ce6c86904c2cbd68b
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-08 03:55:06 -0700

    🔨

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,32 +3,30 @@
 
 /**
  * print_sign - print sign of n
- *
  * @n: int
- *
  * description : return sign according to condition
  *
  * Return:+ if positive, 0 if zero and - if negative
- *
  */
 
 int print_sign(int n)
+
 {
+
 	if (n > 0)
 	{
-		_putchar ('+');
 
+		_putchar ('+');
 		return (1);
 
 	}
-
 	else if (n < 0)
+
 	{
 		_putchar ('-');
 		_putchar (-1);
 
 		return (0);
-
 	}
 
 	else
@@ -36,7 +34,6 @@ int print_sign(int n)
 		_putchar (0);
 
 		return (0);
-
 	}
 }
 
