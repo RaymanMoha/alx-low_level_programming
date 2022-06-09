66212d4ff8123a7f0480a070fe7864d78a2b42fb
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-09 13:38:02 -0700

    a function that checks for uppercase character

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void print_remaining_days(int month, int day, int year);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 #endif /* MAIN_H */
