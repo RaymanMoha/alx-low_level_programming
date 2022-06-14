9553902d2718f5614639f087cb4636d93980e75c
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-14 00:26:27 -0700

    swap

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
  * @s: input string.
  * Return: The length of str.
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int length = 0;
 
-	while (*str++; length++;)
+	while (*(s + length) != '\0')
+		length++;
 
 	return (length);
 }
