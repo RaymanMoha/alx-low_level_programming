3a2b4596148ce7ce376d1a15cbe4f90d34316779
RaymanMoha <abdulmoharayman@gmail.com>
2022-06-20 23:41:57 -0700

    memeset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _memset - ironnou
- * @s: pointer to char params
- * @b: data to change
- * @n: index
- * Return: *s
+ *  _memset - fill n bytes of memory with a constant value
+ *  @s: pointer to memory area
+ *  @b: constant value
+ *  @n: number of bytes to fill
+ *  Return: memory area
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 
