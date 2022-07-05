d82b4a552ad68b78d46c09dc7da2cdc0c9d9b9de
RaymanMoha <abdulmoharayman@gmail.com>
2022-07-05 04:46:52 -0700

    dog bites

--- x0E-structures_typedef/dog.h
+++ x0E-structures_typedef/dog.h
@@ -1,24 +1,9 @@
-#ifndef _DOG_H_
-#define _DOG_H_
-
-/**
- * struct dog - A new type describing a dog.
- * @name: name of the dog
- * @age: age of the dog
- * @owner: ower of the dog
- */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - typedef for struct dog
- */
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -27,4 +12,3 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
-#endif
