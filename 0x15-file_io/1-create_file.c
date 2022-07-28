9f68b371cd9490b36e503043ab8642b5ed88dc58
RaymanMoha <abdulmoharayman@gmail.com>
2022-07-28 04:54:12 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,45 +9,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int o, w, num = 0;
 
-		int o, w, num = 0;
+	if (filename == NULL)
+		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (num = 0; text_content[num];)
+			num++;
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, num);
 
-
-			if (filename == NULL)
-
-						return (-1);
-
-
-
-				if (text_content != NULL)
-
-						{
-
-									for (num = 0; text_content[num];)
-
-													num++;
-
-										}
-
-
-
-					o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-						w = write(o, text_content, num);
-
-
-
-							if (o == -1 || w == -1)
-
-										return (-1);
-
-
-
-								close(o);
-
-
-
-									return (1);
-
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
 }
